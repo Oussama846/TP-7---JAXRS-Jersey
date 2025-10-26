5b00556e2a67d5dc49532fa9d6f87d31596ddcd1
Oussama846 <oussamadarradirma@gmail.com>
2025-10-26 11:33:49 +0100

    Add files via upload

---
+++ TP7_JAXRS_Jersey-main/src/main/java/com/example/tp7_jaxrs_jersey/TypeCompte.java
@@ -0,0 +1,5 @@
+package com.example.tp7_jaxrs_jersey;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ TP7_JAXRS_Jersey-main/src/test/java/com/example/tp7_jaxrs_jersey/Tp7JaxrsJerseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp7_jaxrs_jersey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp7JaxrsJerseyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
