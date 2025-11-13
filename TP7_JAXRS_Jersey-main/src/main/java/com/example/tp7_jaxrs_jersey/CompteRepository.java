db3b06c0691ef7d484c0ac83301f12e7d517c51b
Oussama846 <oussamadarradirma@gmail.com>
2025-11-13 17:39:55 +0100

    Delete TP7_JAXRS_Jersey-main directory

--- TP7_JAXRS_Jersey-main/src/main/java/com/example/tp7_jaxrs_jersey/CompteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.tp7_jaxrs_jersey;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte, Long> {
-}
--- TP7_JAXRS_Jersey-main/src/main/java/com/example/tp7_jaxrs_jersey/TypeCompte.java
+++
@@ -1,5 +0,0 @@
-package com.example.tp7_jaxrs_jersey;
-
-public enum TypeCompte {
-    COURANT, EPARGNE
-}
